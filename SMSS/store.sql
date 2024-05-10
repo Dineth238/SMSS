f65a161eeb095e47b623a7d27f0c0be651553128
Dineth238 <134941851+Dineth238@users.noreply.github.com>
2024-05-10 17:15:37 +0530

    deleted extra file

--- SMSS/store.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE sponsorship_management;
-USE sponsorship_management;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
